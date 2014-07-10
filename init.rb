eafc70213be3f61ce25427fdb61535374b16ff9e
Dariusz Kowalski <darek@76.pl>
2014-07-10 21:52:30 +0200

    Add option to include all redmine members

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_user_mentions do
   name 'Redmine User Mentions'
   author 'Dariusz Kowalski'
   description 'This Redmine plugin gives suggestions on using username in all text-wiki elements. It generates WikiNG compatibile user#name tags and put them in to text'
-  version '1.0.0'
+  version '1.1.0'
   url 'https://github.com/darioo/redmine_user_mentions'
   author_url 'https://github.com/darioo/'
   settings :default => {'trigger' => '@', 'mention_from_all_users' => 0}, :partial => 'settings/mention'
